ff5456e4d3d7841c51b58a16e0021841dbadeca8
Óscar Villa <villaoscarad@gmail.com>
2024-03-12 03:41:38 +0000

    Output as expected, not process

--- filesize_formatter/src/main.rs
+++ filesize_formatter/src/main.rs
@@ -1,32 +1,39 @@
 use std::env;
+use std::collections::HashMap;
 
-enum FileSize {
-    Bytes(u64),
-    Kilobytes(f64),
-    Megabytes(f64),
-    Gigabytes(f64),
-}
 
-fn format_size(size: u64) -> String {
-    let filesize = match size {
-        0..=999 => FileSize::Bytes(size),
-        1000..=999_999 => FileSize::Kilobytes(size as f64 / 1000.0),
-        1_000_000..=999_999_999 => FileSize::Megabytes(size as f64 / 1_000_000.0),
-        _ => FileSize::Gigabytes(size as f64 / 1_000_000_000.0),
+fn format_size(size: f64, xb: &str) -> HashMap<&str, f64> {
+    let size_bytes = match xb {
+        "B" => size,
+        "Kb" => size * 1_024.0,
+        "Mb" => size * 1_024.0 * 1_024.0,
+        "Gb" => size * 1_024.0 * 1_024.0 * 1_024.0,
+        "Tb" => size * 1_024.0 * 1_024.0 * 1_024.0 * 1_024.0,
+        _ => 0.0,
     };
 
-    match filesize {
-        FileSize::Bytes(bytes) => format!("{} bytes", bytes),
-        FileSize::Kilobytes(kb) => format!("{:.2} KB", kb),
-        FileSize::Megabytes(mb) => format!("{:.2} MB", mb),
-        FileSize::Gigabytes(gb) => format!("{:.2} GB", gb),
-    }
+    let mut data = HashMap::new();
+    data.insert("terabytes", size_bytes / (1_024.0 * 1_024.0 * 1_024.0 * 1_024.0));
+    data.insert("gigabytes", size_bytes / (1_024.0 * 1_024.0 * 1_024.0));
+    data.insert("megabytes", size_bytes / (1_024.0 * 1_024.0));
+    data.insert("kilobytes", size_bytes / 1_024.0);
+    data.insert("bytes", size_bytes);
+      
+    return data;
 }
 
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("From CLI catch {}", args[1]);
-    let result = format_size(6888837399);
-    println!("{}", result)
+    let size_xb: &String = &args[1];
+    let size_xb = size_xb.to_string();
+    let size_xb: Vec<&str> = size_xb.split_whitespace().collect();
+    let size = size_xb[0];
+    let size: Result<f64, _> = size.parse();
+    let size: f64 = size.unwrap();
+    let xb = size_xb[1];
+    println!("User input size: {}", size);
+    println!("User input format: {}", xb);
+    let format = format_size(size, xb);
+    println!("sizes: {:?}", format);
 }
